8eb8ef45bde5ab22eadcebfd211d3ebb683556d3
samuelvasquez891 <samuelvasquezespitia@gmail.com>
2024-10-13 12:49:01 -0500

    Create diccionario.py

---
+++ diccionario.py
@@ -0,0 +1,15 @@
+diccionario={
+    "JUICIOSO":"persona responsable",
+    "CHIMBA":"algo que las espectativas o algo bonito",
+    "CRISPETAS":"también conocidas como , rosas, pochoclos, cabritas",
+    "AGUACATE":"también conocido como palta"
+}
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+#print(diccionario.keys())
+if word in diccionario.keys():
+    # ¿Qué debemos hacer si se encuentra la palabra?
+    
+else:
+    # ¿Qué hacer si no se encuentra la palabra?
